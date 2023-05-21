a0a56fc3b01abe0102e9a1927a4bf0bdb296a1bc
anhTuspham <panhtu0902@gmail.com>
2023-05-21 15:42:54 +0700

    Commit

---
+++ Image-processing-AnhTu.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_icon="📷",
+    page_title="Image Processing"
+)
+# st.title("MainPage")
+st.write("# Welcome to my website")
+
+st.sidebar.success("Select Demo")
